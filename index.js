d5e7ad6db760e6b99af0c789b17cf0b72beb3230
muslax <muslax@gmail.com>
2022-02-19 21:52:14 +0700

    v0.0.3

--- index.js
+++ index.js
@@ -542,16 +542,9 @@ const GMATE_ITEMS = {
 }
 
 const GMATE_CONDITIONS = {
-  K01: `<div>
-    <div title="K01" items="3">
-    <p>Divisi Anda telah menunjukkan prestasi terbaik pada tahun ini, dan perusahaan memberikan insentif berupa paket liburan ke sejumlah tujuan wisata dalam dan luar negeri. Biasanya, dalam menentukan siapa yang berhak berwisata di dalam kota, ke luar kota atau yang berhak ke luar negeri, tolok ukur utamanya adalah hierarki jabatan dan masa kerja. Namun Anda berpandangan bahwa prestasi kerja (nilai kinerja) merupakan kriteria utama, diikuti dengan yang kedua: hierarki jabatan, lalu gaji dan yang terakhir masa kerja. Untuk membantu Anda mengambil keputusan, Anda telah menentukan bahwa prestasi dua kali lebih penting daripada gaji, dan masa kerja hanya setengah pentingnya dari gaji. Jawablah pertanyaan-pertanyaan berikut berdasarkan informasi di bawah ini.</p>
-    <figure><img src="" title="Tabel dan Grafik Hierarki Jabatan"/></figure>
-  </div>`,
+  K01: `<div> <div title="K01" items="3"> <p>Divisi Anda telah menunjukkan prestasi terbaik pada tahun ini, dan perusahaan memberikan insentif berupa paket liburan ke sejumlah tujuan wisata dalam dan luar negeri. Biasanya, dalam menentukan siapa yang berhak berwisata di dalam kota, ke luar kota atau yang berhak ke luar negeri, tolok ukur utamanya adalah hierarki jabatan dan masa kerja. Namun Anda berpandangan bahwa prestasi kerja (nilai kinerja) merupakan kriteria utama, diikuti dengan yang kedua: hierarki jabatan, lalu gaji dan yang terakhir masa kerja. Untuk membantu Anda mengambil keputusan, Anda telah menentukan bahwa prestasi dua kali lebih penting daripada gaji, dan masa kerja hanya setengah pentingnya dari gaji. Jawablah pertanyaan-pertanyaan berikut berdasarkan informasi di bawah ini.</p> <figure><img src="" title="Tabel dan Grafik Hierarki Jabatan"/></figure> </div>`,
 
-  K02: `<div>
-    <div title="K02" items="1">
-    <figure><img src="" title="Keuntungan dari 5 Perusahaan Sejenis"/></figure>
-  </div>`,
+  K02: `<div> <div title="K02" items="1"> <figure><img src="" title="Keuntungan dari 5 Perusahaan Sejenis"/></figure> </div>`,
 
   K03: `<div>
     <div title="K03" items="1">
